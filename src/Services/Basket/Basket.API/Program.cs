bf7d4cd60e850fae70da05e3ee5ade2d485213d7
Ruhollah <Ruhollah@gmail.com>
2022-06-08 15:01:49 +0430

    Basket Added

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,14 +1,17 @@
-using Catalog.API.Data;
-using Catalog.API.Data.Interfaces;
-using Catalog.API.Repositories;
-using Catalog.API.Repositories.Interfaces;
+using Basket.API.Repositories;
+using Basket.API.Repositories.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddScoped<ICatalogContext, CatalogContext>();
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
+// Redis Configuration
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetValue<string>("CacheSettings:ConnectionString");
+});
+
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
